e0eadc63f13165b9bc4e4347eee857caf8aac42b
Shivashankar-gitH <shiva1894300@gmail.com>
2020-12-21 20:28:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import image from "./images/breakingBad.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [data, setData] = React.useState([]);
+
+	React.useEffect(() => {
+		console.log("hooks working");
+		fetch(`https://breakingbadapi.com/api/characters`)
+			.then((response) => response.json())
+			.then((result) => setData(result));
+	}, []);
+
+	const characterName = (e) => {
+		console.log(e.target.value);
+		fetch(`https://breakingbadapi.com/api/characters?name=${e.target.value}`)
+			.then((response) => response.json())
+			.then((result) => setData(result));
+	};
+
+	return (
+		<div className="App">
+			<div className="app--img">
+				<img src={image} alt="" className="app--img__in" />
+			</div>
+			<div className="inputField">
+				<input
+					type="text"
+					placeholder="Search characters..."
+					className="inputField--in"
+					onChange={characterName}
+				/>
+			</div>
+			<ul className="cards">
+				{data.map((el, idx) => (
+					<li className="flip-card" key={idx}>
+						<div className="flip-card-inner">
+							<div className="flip-card-front">
+								<img src={el.img} alt="characters" className="flip-card-img" />
+							</div>
+							<div className="flip-card-back">
+								<h1>{el.name}</h1>
+								<h3>Nickname:{el.nickname}</h3>
+								<h3>status:{el.status}</h3>
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export default App;
